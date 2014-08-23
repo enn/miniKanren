8a6af5a0f8c7fc30b0d72c8c51ec749a5c0ba9b0
enn <enn@users.noreply.github.com>
2014-08-23 19:42:56 +0100

    Update mk.scm
    Fix the minikanren code so that it works in chicken scheme.

--- mk.scm
+++ mk.scm
@@ -1,3 +1,22 @@
+(define remp
+  (lambda (p ls)
+    (cond
+     ((null? ls) '())
+     ((p (car ls)) (remp p (cdr ls)))
+     (else (cons (car ls) (remp p (cdr ls)))))))
+
+(define (exists p ls)
+  (cond ((null? ls) #f)
+        ((p (car ls)) #t)
+        (else (exists p (cdr ls)))))
+
+(define sorter (lambda (ls) (sort ls lex<=?)))
+
+(define datum->string
+  (lambda (x)
+    (with-output-to-string (lambda () (display x)))))
+
+
 (define c->S (lambda (c) (car c)))
 
 (define c->D (lambda (c) (cadr c)))
@@ -640,7 +659,7 @@
              `(absent ,tag ,x)))
          T)))
 
-(define sorter (lambda (ls) (sort lex<=? ls)))
+;(define sorter (lambda (ls) (sort lex<=? ls)))
 
 (define sort-part
   (lambda (pr)
